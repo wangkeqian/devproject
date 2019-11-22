669a92fb9d900b03a5c4de235ad9fe92a681f9db
WangKeqian <1079095540@qq.com>
2019-11-22 23:56:27 +0800

    eee

---
+++ demo-test-github/src/main/java/com/test/demotestgithub/DemoTestGithubApplication.java
@@ -0,0 +1,13 @@
+package com.test.demotestgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestGithubApplication.class, args);
+    }
+
+}
---
+++ demo-test-github/src/test/java/com/test/demotestgithub/DemoTestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.demotestgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
